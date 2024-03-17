d6fc2bbf9e2e2f09b42a60af2df2797849bd46fc
Alex Wu <alexex00001@hotmail.com>
2024-03-17 18:55:08 -0400

    add in TypeScript and update app file

--- app.ts
+++ app.ts
@@ -1,8 +1,9 @@
+import { Request, Response } from "express";
 const express = require('express');
 const port = 3000;
 
 const app = express();
-app.get('/', (req, res) => {
+app.get('/', (req : Request, res : Response) => {
   res.send('Hello World!');
 });
 
